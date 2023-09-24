d3b5e055dc42e5e38210639253215141e520423a
Jaime Baeza Casado <jbaeza-c@c2r16s5.42madrid.com>
2023-09-24 21:04:27 +0200

    Minor changes

--- CommonCore/push_swap/srcs/commands/rotate.c
+++ CommonCore/push_swap/srcs/commands/rotate.c
@@ -6,8 +6,43 @@
 /*   By: jimmy <jbaeza-c@student.42madrid.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 13:19:19 by jimmy             #+#    #+#             */
-/*   Updated: 2023/09/24 13:19:46 by jimmy            ###   ########.fr       */
+/*   Updated: 2023/09/24 19:10:25 by jbaeza-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../includes/psw.h"
+
+static void	rotate(t_stack **stack)
+{
+	t_node	*node;
+	t_node	*last;
+
+	node = (*stack)->node;
+	if (!node || !node->next)
+		return ;
+	(*stack)->node = node->next;
+	node->next->prev = NULL;
+	last = get_last(&node);
+	last->next = node;
+	node->next = NULL;
+	node->prev = last;
+}
+
+void	do_ra(t_stack **stack_a)
+{
+	rotate(stack_a);
+	ft_printf("ra\n");
+}
+
+void	do_rb(t_stack **stack_b)
+{
+	rotate(stack_b);
+	ft_printf("rb\n");
+}
+
+void	do_rr(t_stack **stack_a, t_stack **stack_b)
+{
+	rotate(stack_a);
+	rotate(stack_b);
+	ft_printf("rr\n");
+}
