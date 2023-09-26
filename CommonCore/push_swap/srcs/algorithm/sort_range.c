4862cde9943e5f2714d5f2762bcc27b2c002d82b
Jaime Baeza Casado <jbaeza-c@c3r12s1.42madrid.com>
2023-09-26 19:51:36 +0200

    Finishing psw

--- CommonCore/push_swap/srcs/algorithm/sort_range.c
+++ CommonCore/push_swap/srcs/algorithm/sort_range.c
@@ -1,20 +1,69 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   jimmy_sort.c                                       :+:      :+:    :+:   */
+/*   sort_range.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jimmy <jbaeza-c@student.42madrid.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 11:43:52 by jimmy             #+#    #+#             */
-/*   Updated: 2023/09/26 14:17:42 by jimmy            ###   ########.fr       */
+/*   Updated: 2023/09/26 19:16:20 by jbaeza-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../includes/psw.h"
 
+void	sort_b(t_stack **stack)
+{
+	t_node		*node;
+	int			cnt;
+	static int	mid;
+
+	node = (*stack)->node;
+	cnt = 0;
+	mid = (*stack)->size / 2;
+	while (node->next)
+	{
+		if (node->index < node->next->index)
+		{
+			if (cnt <= mid)
+				do_rb(stack);
+			else
+				do_rrb(stack);
+		}
+		node = node->next;
+		cnt++;
+	}
+}
+
 void	align_b(t_stack **stack_a, t_stack **stack_b)
 {
 	int	index;
+	int	is_in_stack;
+	int	up;
+	int	down;
+
+	if ((*stack_b)->node = NULL)
+		return ;
+	index = (*stack_a)->node->index;
+	is_in_stack = 0;
+	while (index > 0 && !is_in_stack)
+		if (find_index(stack_b, --index))
+			is_in_stack = 1;
+	if (!is_in_stack)
+		sort_b(stack_b);
+	else
+	{
+		up = search_first(stack_b, index, index);
+		down = search_last(stack_b, index, index);
+		if (up <= down)
+			while (up--)
+				do_rb(stack_b);
+		else
+			while (down--)
+				do_rb(stack_b);
+	}
+}
+
 
 
 
@@ -24,7 +73,7 @@ void	sort_range(t_stack **stack_a, t_stack **stack_b, int max, int min)
 	int	down;
 	int	i;
 
-	while (search_first(stack_a, max, min) > -1)
+	while (search_first(stack_a, max, min) >= 0)
 	{
 		i = 0;
 		up = search_first(stack_a, max, min);
