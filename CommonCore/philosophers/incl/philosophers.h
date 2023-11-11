d9caa82eea61588f76c1a6324c502fbfdfc2ebfe
jimmy <jbaeza-c@student.42madrid.com>
2023-11-11 01:30:25 +0100

    Philosophers

---
+++ CommonCore/philosophers/incl/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbaeza-c <jbaeza-c@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 01:24:44 by jbaeza-c          #+#    #+#             */
+/*   Updated: 2023/11/11 01:28:55 by jbaeza-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
